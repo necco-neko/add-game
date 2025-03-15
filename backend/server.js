0365d7659d3a8b1d7d20b9308d378bfd7ce97f4c
necco-neko <nekonecconeko@gmail.com>
2025-03-15 18:05:09 +0900

    デプロイ準備1

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const pexelsRouter = require("./routes/pexelsRouter");
 const rankingRouter = require("./routes/rankingRouter");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +20,12 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use("/api/pexels", pexelsRouter);
 app.use("/api/rankings", rankingRouter);
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
